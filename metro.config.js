7bc89eaa6835a842233e8355992d8781bc5c6384
Trevor Lam <204136860+TrevorPLam@users.noreply.github.com>
2026-01-20 15:02:30 +0000

    asd

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-// Mobile-only app - iOS and Android only, no web
-// This ensures Metro only bundles for mobile platforms
-config.resolver.platforms = ["ios", "android"];
-
-module.exports = config;
