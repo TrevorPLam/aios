acd3b0105db494880bbb0d6c98eaa39f00075c1e
Trevor Lam <204136860+TrevorPLam@users.noreply.github.com>
2026-01-30 18:05:46 -0600

    chore(features): ignore ui in eslint

--- packages/features/.eslintrc.js
+++ packages/features/.eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
   extends: ["@aios/eslint-config/react"],
+  ignorePatterns: ["**/__tests__/**", "**/ui/**"],
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    "import/default": "warn",
+    "import/order": "warn",
+    "import/export": "warn",
+    "prefer-const": "warn",
+    "@typescript-eslint/no-duplicate-enum-values": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-namespace": "warn",
+  },
 };
